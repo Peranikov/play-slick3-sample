1dadd04bd0682c16fc9b1db1f740c281f4d4f85b
matsukubo <matsukubo@email.com>
2015-08-20 17:20:53 +0900

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import models.Cat
+import play.api.Play
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.data.Forms.text
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfig
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import slick.driver.JdbcProfile
+import tables.CatTable
+
+class Application extends Controller with CatTable with HasDatabaseConfig[JdbcProfile] {
+  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+  import driver.api._
+
+  val Cats = TableQuery[Cats]
+
+  def index = Action.async {
+    db.run(Cats.result).map(res => Ok(views.html.index(res.toList)))
+  }
+
+  val catForm = Form(
+    mapping(
+      "name" -> text(),
+      "color" -> text()
+    )(Cat.apply)(Cat.unapply)
+  )
+
+  def insert = Action.async { implicit rs =>
+    val cat = catForm.bindFromRequest.get
+    db.run(Cats += cat).map(_ => Redirect(routes.Application.index))
+  }
+}
